7bedf9eb64713bd979753832f926baa83f5506b1
LizbethOrtizLopez <A00227346@itesm.mx>
2020-09-23 01:28:33 +0000

    practica 3

---
+++ practicas/3/creaprocesos.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv){
+    unsigned pid = fork();
+    if (pid==0){
+        printf("Soy el proceso hijo\n");
+        execv(argv[1],&argv[1]);
+        return 10;
+    }
+    else{
+        int status;
+        printf("Soy el proceso padre y mi hijo es %u\n",pid);
+        wait(&status);
+        printf("Terminando despues del hijo y estatus %u\n",status);
+    }
+    printf("Hola mundo\n");
+    return 0;
+}
